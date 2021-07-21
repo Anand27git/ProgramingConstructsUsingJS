747c71f8c2af00b2d4008e687a431b4b6f614784
Anand27git <anandkumar256k@gmail.com>
2021-07-21 23:55:11 +0530

    unit hundered thousand conversion

---
+++ numbers.js
@@ -0,0 +1,12 @@
+// Problem 3  number to word unit hundered thousand conversion
+{
+    let number = prompt("Enter any of numbers 1, 10, 100, 1000 ---- ");
+    if (number == 1)
+        console.log("unit");
+    else if (number == 10)
+        console.log("ten");
+    else if (number == 100)
+        console.log("hundred");
+    else if (number == 1000)
+        console.log("thousand");
+}
