6db0c9b2fc3f6ee83ecfd5b573fd01a21e45d4f8
Anand27git <anandkumar256k@gmail.com>
2021-07-21 09:56:51 +0530

    leap year

---
+++ Leapyear.js
@@ -0,0 +1,15 @@
+/* finding the leap year using java script */
+
+    let year = prompt("Enter the year: ");
+    let isLeapYear = false;
+    if (year > 999 && year <= 9999) {
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                isLeapYear = true;
+            }
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
+    }
+    console.log("the given leap year  is- " + isLeapYear);
